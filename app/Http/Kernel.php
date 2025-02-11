4c61df9a4b7158fc8ddc9f8e2475ad40f68bcc1b
Kareem5200 <karim8862@gmail.com>
2025-02-11 16:35:45 +0200

    Third Commit

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,7 +65,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'checkPostID'=>\App\Http\Middleware\CheckExistsingPostID::class,
-        'checkPost'=>\App\Http\Middleware\CheckExistsingPost::class,
 
     ];
 }
