0b03d15ea2c136acb15222d45f24f38a0da0936d
Kareem5200 <karim8862@gmail.com>
2025-02-11 15:53:49 +0200

    Third Commit

---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comment>
+ */
+class CommentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'comment'=>Str::random(10),
+            'user_id'=>User::inRandomOrder()->first()->id,
+            'commentable_type'=>Post::class,
+            'commentable_id'=>Post::inRandomOrder()->first()->id,
+
+        ];
+    }
+}
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,8 +27,9 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'password' =>'kareem2000$',
+            'profile_image'=> Str::random(10),
+            'bio'=>fake()->jobTitle(),
         ];
     }
 
